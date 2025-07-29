e5f7a2419a784202e1bf1a5b3ba5ddf71758d57f
Anakin <hotgluehacks@gmail.com>
2025-07-29 12:21:20 -0700

    Max of 2, solved!

---
+++ maxof2.py
@@ -0,0 +1,12 @@
+def main():
+    x = int(input())
+    y = int(input())
+    if x > y:
+        print(f"Max of {x} and {y} is {x}")
+    elif y > x:
+        print(f"Max of {x} and {y} is {y}")
+    else:
+        print(f"Max of {x} and {y} is {y}")
+ 
+
+main()
