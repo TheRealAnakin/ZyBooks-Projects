005e9372a07b3411d49cd8f571db43c43027f087
Anakin <hotgluehacks@gmail.com>
2025-08-05 17:42:10 -0700

    SOLVED MEDIANOF3!

--- medianof3.py
+++ medianof3.py
@@ -11,7 +11,30 @@ def main():
     for i in range(0, len(arr) - 1):
         if arr[i] > arr[i + 1]:
             biggest = arr[i]
-        
-        
-    print("Biggest:", biggest)
+    if z > x:
+        if z > y:
+            biggest = z
+    if x > z:
+        if x > y:
+            biggest = x
+    if y > x:
+        if y > z:
+            biggest = y
+    if x < z:
+        if x < y:
+            smallest = x
+    if z < y:
+        if z < x:
+            smallest = z
+    if y < x:
+        if y < z:
+            smallest = y
+    for i in range(0, len(arr) - 1):
+        if arr[i] != biggest:
+            if arr[i] != smallest:
+                median = arr[i]
+    if arr[len(arr) - 1] != biggest:
+        if arr[len(arr)-1] != smallest:
+            median = arr[len(arr)-1]
+    print(median)
 main()
